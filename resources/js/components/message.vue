5355c15c39f2d6c31f31283f1c5e37f694753c0d
farhadrohani <farhad.3.rohani@gmail.com>
2020-06-09 05:27:08 +0430

    ایجاد چت روم

---
+++ resources/js/components/message.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="col-lg-12">
+    <div>
+        <textarea name="" id="" cols="30" rows="10" v-model="message"></textarea>
+        <input type="button" value="ارسال پیام" @click="sendMessage" class="btn btn-info">
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "message",
+        methods:{
+            sendMessage() {
+axios.post('/sendMessage')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
